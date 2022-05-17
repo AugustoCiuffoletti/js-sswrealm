8a1834f61e2185992ad5cbdc9edfd2fa5e4646f5
AugustoCiuffoletti <augusto@di.unipi.it>
2022-05-17 11:02:27 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import "./style.css";
 
 function download() {
   let URL =
-    "https://data.mongodb-api.com/app/temperature-lwkwk/endpoint/cerca0?n=";
+    "https://data.mongodb-api.com/app/temperature-lwkwk/endpoint/cerca?n=";
   fetch(URL + document.getElementById("input").value)
     .then(response => response.json(), error => alert(error))
-    .then(data => (document.getElementById("temp").innerHTML = data.temperature));
+    .then(data => (document.getElementById("temp").innerHTML = data));
 }
 
 document.getElementById("chiedi").addEventListener("click", download);
