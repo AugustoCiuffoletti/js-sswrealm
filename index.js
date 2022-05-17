8d948ace7f5b3b34cb008e6a999a3ee175016c83
AugustoCiuffoletti <augusto@di.unipi.it>
2022-05-17 11:10:38 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
 // Import stylesheets
-import "./style.css";
+import './style.css';
 
-var URL =
-  "https://webhooks.mongodb-realm.com/api/client/v2.0/app/temperature-lwkwk/service/temperature/incoming_webhook/";
+var URL = 'https://data.mongodb-api.com/app/temperature-lwkwk/endpoint/';
 
-var cities=[];
+var cities = [];
 
 function $(s) {
   return document.getElementById(s);
 }
 
 function download() {
-  fetch(URL + "aggiornato?n=" + $("input").value)
-    .then(response => response.json(), error => alert(error))
-    .then(data => {
-      console.log(data)
-      $("temp").innerHTML = data
+  fetch(URL + 'aggiornato?n=' + $('input').value)
+    .then(
+      (response) => response.json(),
+      (error) => alert(error)
+    )
+    .then((data) => {
+      console.log(data);
+      $('temp').innerHTML = data;
     });
 }
 
 function insert() {
-  let newCity = $("nuovo").value;
-  if ( newCity === "" ) return;
-  fetch(URL + "inserisci?n=" + newCity)
-    .then(response => response.json(), error => alert(error))
-    .then( () => {
+  let newCity = $('nuovo').value;
+  if (newCity === '') return;
+  fetch(URL + 'inserisci?n=' + newCity)
+    .then(
+      (response) => response.json(),
+      (error) => alert(error)
+    )
+    .then(() => {
       cities.push(newCity);
       refreshList();
     });
 }
 
 function refreshList() {
-  $("cityList").innerHTML = "";
-  cities.forEach(c => ($("cityList").innerHTML += "<li> " + c));
+  $('cityList').innerHTML = '';
+  cities.forEach((c) => ($('cityList').innerHTML += '<li> ' + c));
 }
 
-$("chiedi").addEventListener("click", download);
-$("inserisci").addEventListener("click", insert);
+$('chiedi').addEventListener('click', download);
+$('inserisci').addEventListener('click', insert);
 
-fetch(URL + "elenco")
-  .then(response => response.json(), error => alert(error))
-  .then(data => { cities = data; refreshList()} );
+fetch(URL + 'elenco')
+  .then(
+    (response) => response.json(),
+    (error) => alert(error)
+  )
+  .then((data) => {
+    cities = data;
+    refreshList();
+  });
