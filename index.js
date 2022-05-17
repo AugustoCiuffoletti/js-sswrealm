f55e0605a494dc15f77571670442e7ca3e3fef6d
AugustoCiuffoletti <augusto@di.unipi.it>
2022-05-17 11:03:56 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 // Import stylesheets
-import "./style.css";
+import './style.css';
 
-var URL =
-  "https://webhooks.mongodb-realm.com/api/client/v2.0/app/temperature-lwkwk/service/temperature/incoming_webhook/";
+var URL = 'https://data.mongodb-api.com/app/temperature-lwkwk/endpoint/';
 
 var cities;
 
-function $(s) { return document.getElementById(s)}
-
-function download() {
-  fetch(URL + "cerca?n=" + $("input").value)
-    .then(response => response.json(), error => alert(error))
-    .then(data => 
-      $("temp").innerHTML = data );
+function $(s) {
+  return document.getElementById(s);
 }
 
-$("chiedi").addEventListener("click", download);
+function download() {
+  fetch(URL + 'cerca?n=' + $('input').value)
+    .then(
+      (response) => response.json(),
+      (error) => alert(error)
+    )
+    .then((data) => ($('temp').innerHTML = data));
+}
 
-fetch(URL + "elenco" + $("input").value)
-  .then(response => response.json(), error => alert(error))
-   .then(data =>
-    data.forEach( c => $("cityList").innerHTML += "<li> " + c )
-    );
+$('chiedi').addEventListener('click', download);
 
+fetch(URL + 'elenco' + $('input').value)
+  .then(
+    (response) => response.json(),
+    (error) => alert(error)
+  )
+  .then((data) =>
+    data.forEach((c) => ($('cityList').innerHTML += '<li> ' + c))
+  );
